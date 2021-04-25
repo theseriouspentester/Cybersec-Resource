514f73c07e6bd0e5855258665552126e80fcef31
KodeYash <yash.arora1045@gmail.com>
2021-04-25 17:05:54 +0530

    burger button

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+const navslide = () =>{
+    const burger= document.querySelector('.burger');
+    const nav=document.querySelector('.nav-links');
+    const navLinks=document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        // Toggle nav
+        nav.classList.toggle('nav-active');
+        // Animate links    
+        navLinks.forEach((link,index) => {
+            if(link.style.animation
+                )
+                {
+                    link.style.animation='';
+                }
+                else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+const app=()=>{
+    navslide();
+}
+app();
